19b1c5c0bc75b3f962dbf45cbd44afc5ec4bc058
Bianca da Silva Campos <51832213+BiancaSCampos@users.noreply.github.com>
2021-09-08 12:54:34 -0300

    First Commit

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,7 +1,25 @@
+import { createContext, useState } from 'react';
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from "./Pages/Home";
+import { NewRoom } from "./Pages/NewRoom";
+
+export const AuthContext = createContext({} as any)
+
 function App() {
+
+  const [user, setUser] = useState()
+
   return (
-    <h1>Hello World</h1>
-  );
+    <BrowserRouter>
+
+      <AuthContext.Provider value={{ user, setUser }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContext.Provider>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ letmeask/src/Pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from "../Assets/images/illustration.svg";
+import logoImg from "../Assets/images/logo.svg";
+import "../Styles/auth.scss"
+import { Button } from "../Components/Button";
+import { useContext } from "react";
+import { TestContext } from "../App";
+
+export function NewRoom() {
+
+ 
+
+  return (
+    <div id= "page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong> Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
